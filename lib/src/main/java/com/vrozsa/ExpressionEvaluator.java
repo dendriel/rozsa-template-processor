0357fcd6364af5d2e194ea11216e6fccfdaf2900
Vitor Rozsa <vitor.rozsa@hotmail.com>
2025-02-10 07:19:29 -0300

    Clean code

---
+++ lib/src/main/java/com/vrozsa/ExpressionEvaluator.java
@@ -0,0 +1,61 @@
+package com.vrozsa;
+
+import java.util.List;
+
+class ExpressionEvaluator {
+    private final ContextHolder context;
+    private final List<Expression> expressions;
+    private final String contentAsText;
+
+    ExpressionEvaluator(ContextHolder context, List<Expression> expressions, char[] content) {
+        this(context, expressions, String.copyValueOf(content));
+    }
+
+    ExpressionEvaluator(ContextHolder context, List<Expression> expressions, String contentAsText) {
+        this.context = context;
+        this.expressions = expressions;
+        this.contentAsText = contentAsText;
+    }
+
+    /**
+     * Build the new content with the result of the evaluated expressions.
+     * @return the evaluated content.
+     */
+    public String evaluate() {
+        evaluateExpressions();
+
+        return buildEvaluatedContent();
+    }
+
+    private void evaluateExpressions() {
+        expressions.forEach(e -> e.evaluate(context));
+    }
+
+    private String buildEvaluatedContent() {
+
+        var newContent = new StringBuilder();
+
+        var contentIdx = 0;
+
+        for (var currExp : expressions) {
+            // -1 for the $
+            var startIdx = currExp.startIdx() - 1;
+
+            var segment = contentAsText.substring(contentIdx, startIdx);
+            newContent.append(segment);
+
+            var expValue = currExp.getResult();
+            newContent.append(expValue);
+            // +1 for the $ and +1 to get to the next char after the expression
+            contentIdx += segment.length() + currExp.length() + 2;
+        }
+
+        // Add the rest of the content
+        var lastExp = expressions.get(expressions.size() - 1);
+
+        var lastSegment = contentAsText.substring(lastExp.endIdx() + 1);
+        newContent.append(lastSegment);
+
+        return newContent.toString();
+    }
+}
--- lib/src/main/java/com/vrozsa/Main.java
+++ lib/src/main/java/com/vrozsa/Main.java
@@ -2,8 +2,6 @@ package com.vrozsa;
 
 public class Main {
     public static void main(String[] args) {
-
-
         var content = new FileReader().readFile("sample_template_0.yml").toCharArray();
 
         var context = ContextHolder.create()
@@ -15,33 +13,11 @@ public class Main {
         var expressions = ExpressionScanner.scan(0, content);
         System.out.println("Scanned content: " + expressions);
 
+        var contentEvaluator = new ExpressionEvaluator(context, expressions, content);
+        var evaluatedContent = contentEvaluator.evaluate();
 
-        // this logic may go in the ExpressionEvaluator
-        expressions.forEach(e -> e.evaluate(context));
 
-        var newContent = new StringBuilder();
-        var contentAsText = String.copyValueOf(content);
-
-        var contentIdx = 0;
-
-        for (var currExp : expressions) {
-            var startIdx = currExp.startIdx() - 1; // -1 for the $
-
-            var segment = contentAsText.substring(contentIdx, startIdx);
-            newContent.append(segment);
-
-            var expValue = currExp.getResult();
-            newContent.append(expValue);
-            contentIdx += segment.length() + currExp.length() + 2; // next char after expression
-        }
-
-        // Add the rest of the content
-        var lastExp = expressions.get(expressions.size() - 1);
-
-        var lastSegment = contentAsText.substring(lastExp.endIdx() + 1);
-        newContent.append(lastSegment);
-
-        System.out.println("\n\n\n\nEvaluated Content:\n" + newContent);
+        System.out.println("\n\n\n\nEvaluated Content:\n" + evaluatedContent);
 
     }
 
