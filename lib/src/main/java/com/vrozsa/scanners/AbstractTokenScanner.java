2eae1c5adca5bc57f6620d2a2bdb932b68c9f7de
Vitor Rozsa <vitor.rozsa@hotmail.com>
2025-02-22 19:50:22 -0300

    Allow to read text literals (WIP)

--- lib/src/main/java/com/vrozsa/scanners/AbstractTokenScanner.java
+++ lib/src/main/java/com/vrozsa/scanners/AbstractTokenScanner.java
@@ -19,17 +19,23 @@ abstract class AbstractTokenScanner {
     private static final CharacterChecker startingCharsChecker = CharacterChecker.of(
             new CharacterRange('A', 'Z'),
             new CharacterRange('a', 'z'),
-            new CharacterRange('0', '9'),
+//            new CharacterRange('0', '9'),
             new CharacterSingle('<'),
             new CharacterSingle('='),
             new CharacterSingle('>'),
-            new CharacterSingle('!')
+            new CharacterSingle('!'),
+            new CharacterSingle('"')
     );
 
     private static final CharacterChecker literalStartingCharsChecker = CharacterChecker.of(
             new CharacterRange('0', '9'),
             new CharacterSingle('"')
     );
+
+    private static final CharacterChecker literalTextMarkCharChecker = CharacterChecker.of(
+            new CharacterSingle('"')
+    );
+
 
     private static final CharacterChecker middleCharsChecker = CharacterChecker.of(
             new CharacterRange('A', 'Z'), // TODO: use classes to define default ranges
@@ -59,9 +65,11 @@ abstract class AbstractTokenScanner {
         var nextIdx = startIdx;
 
         var nextChar = content[nextIdx++];
-        if (!startingCharsChecker.match(nextChar)) {
+        if (!startingCharsChecker.match(nextChar) && !literalStartingCharsChecker.match(nextChar)) {
             return handleInvalidStartingChar(startIdx, content);
         }
+
+        var isLiteral = literalStartingCharsChecker.match(nextChar);
 
         var keywordBuilder = new StringBuilder();
         keywordBuilder.append(nextChar);
@@ -69,6 +77,16 @@ abstract class AbstractTokenScanner {
         while (nextIdx < content.length) {
 
             nextChar = content[nextIdx];
+
+            // Include the text mark char only if we are scanning a literal Text. Otherwise we may scan a stand-alone
+            // context variable that happens to be followed by the quote as in `"$ctx_variable"` and it would be read as
+            // `ctx_variable"`
+            if (literalTextMarkCharChecker.match(nextChar) && isLiteral) {
+                keywordBuilder.append(nextChar);
+                nextIdx++;
+                break;
+            }
+
             if (!middleCharsChecker.match(nextChar)) {
                 break;
             }
